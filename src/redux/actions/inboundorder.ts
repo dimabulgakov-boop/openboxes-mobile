ede807fd83d5ebd967109ce7fe5b63b7778978a5
druchniewicz <51370022+druchniewicz@users.noreply.github.com>
2025-06-06 15:00:41 +0200

    OB-3231: Creating receiving bin automaticaly during receiving, other … (#304)
    * OB-3231: Creating receiving bin automaticaly during receiving, other small adjustments
    
    * OB-3231: Adjustments after code review
    
    * OB-3231: Code review part2

--- src/redux/actions/inboundorder.ts
+++ src/redux/actions/inboundorder.ts
@@ -7,6 +7,9 @@ export const FETCH_PARTIAL_RECEIVING_SUCCESS = 'FETCH_PARTIAL_RECEIVING_SUCCESS'
 export const SUBMIT_PARTIAL_RECEIVING_REQUEST = 'SUBMIT_PARTIAL_RECEIVING_REQUEST';
 export const SUBMIT_PARTIAL_RECEIVING_SUCCESS = 'SUBMIT_PARTIAL_RECEIVING_SUCCESS';
 
+export const CREATE_RECEIVING_BIN_LOCATION_REQUEST = 'CREATE_RECEIVING_BIN_LOCATION_REQUEST';
+export const CREATE_RECEIVING_BIN_LOCATION_SUCCESS = 'CREATE_RECEIVING_BIN_LOCATION_SUCCESS';
+
 export function fetchInboundOrderList(callback: (data: any) => void, id?: any) {
   return {
     type: FETCH_INBOUND_ORDER_LIST_REQUEST,
@@ -27,6 +30,14 @@ export function submitPartialReceiving(id: any, body: any, callback?: (data: any
   return {
     type: SUBMIT_PARTIAL_RECEIVING_REQUEST,
     payload: { id, body },
+    callback
+  };
+}
+
+export function createReceivingBin(id: any, callback?: (data: any) => void) {
+  return {
+    type: CREATE_RECEIVING_BIN_LOCATION_REQUEST,
+    payload: { id },
     callback
   };
 }
--- src/redux/sagas/inbound.ts
+++ src/redux/sagas/inbound.ts
@@ -5,7 +5,9 @@ import {
   FETCH_PARTIAL_RECEIVING_SUCCESS,
   FETCH_PARTIAL_RECEIVING_REQUEST,
   SUBMIT_PARTIAL_RECEIVING_REQUEST,
-  SUBMIT_PARTIAL_RECEIVING_SUCCESS
+  SUBMIT_PARTIAL_RECEIVING_SUCCESS,
+  CREATE_RECEIVING_BIN_LOCATION_SUCCESS,
+  CREATE_RECEIVING_BIN_LOCATION_REQUEST
 } from '../actions/inboundorder';
 import { hideScreenLoading, showScreenLoading } from '../actions/main';
 import * as api from '../../apis';
@@ -61,6 +63,25 @@ function* submitPartialReceiving(action: any) {
   }
 }
 
+function* createReceivingBinLocation(action: any) {
+  try {
+    yield put(showScreenLoading('Please wait..'));
+    const response: any = yield call(
+      api.createReceivingBinLocation,
+      action.payload.id,
+    );
+    yield put({
+      type: CREATE_RECEIVING_BIN_LOCATION_SUCCESS,
+      payload: response.data
+    });
+    if (action.callback) yield action.callback(response);
+    yield put(hideScreenLoading());
+  } catch (e) {
+    Alert.alert(e.message);
+    yield put(hideScreenLoading());
+  }
+}
+
 function* fetchPartialReceiving(action: any) {
   try {
     yield put(showScreenLoading('Please wait...'));
@@ -84,4 +105,5 @@ export default function* watcher() {
   yield takeLatest(FETCH_INBOUND_ORDER_LIST_REQUEST, fetchInboundOrderList);
   yield takeLatest(FETCH_PARTIAL_RECEIVING_REQUEST, fetchPartialReceiving);
   yield takeLatest(SUBMIT_PARTIAL_RECEIVING_REQUEST, submitPartialReceiving);
+  yield takeLatest(CREATE_RECEIVING_BIN_LOCATION_REQUEST, createReceivingBinLocation)
 }
